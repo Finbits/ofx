4b2b7edd2bf4a70d1559391fcd63ecc6ab82b593
Daniel Lima <daniel.lima@finbits.com.br>
2022-11-16 12:25:08 -0300

    Bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Ofx.MixProject do
   use Mix.Project
 
-  @version "0.0.9"
+  @version "0.0.10"
   @description "A lib to parse and generate OFX data"
   @links %{"GitHub" => "https://github.com/danielwsx64/ofx"}
 
